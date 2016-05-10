518e081740749442b89484b984f9cea761d567f5
Christian Brungart <christian.brungart@gmail.com>
2016-05-10 08:08:05 -0500

    README
    My code is that of a simple platform jumping game. The player navigates
    away from enemies to the end goal. If they get hit by an enemy they are
    teleported back to the start and lose a life.

---
+++ Menu.py
@@ -0,0 +1,85 @@
+import pygame
+ 
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+MARGIN = 5
+grid = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell 
+ 
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(0,0,0), font=None, font_size=30,
+                    font_color=(255, 255, 255)):
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+ 
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+ 
+        self.items = items
+        self.font = pygame.font.SysFont(font, font_size)
+        self.font_color = font_color
+ 
+        self.items = []
+        for index, item in enumerate(items):
+            label = self.font.render(item, 1, font_color)
+ 
+            width = label.get_rect().width
+            height = label.get_rect().height
+ 
+            posx = (self.scr_width / 2) - (width / 2)
+            # t_h: total height of text block
+            t_h = len(items) * height
+            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+ 
+            self.items.append([item, label, (width, height), (posx, posy)])
+ 
+    def run(self):
+        mainloop = True
+        while mainloop:
+            # Limit frame speed to 50 FPS
+            self.clock.tick(50)
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mainloop = False 
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # User clicks the mouse. Get the position
+                    pos = pygame.mouse.get_pos()
+                    # Change the x/y screen coordinates to grid coordinates
+                    column = pos[0] // (SCREEN_WIDTH + MARGIN)
+                    row = pos[1] // (SCREEN_HEIGHT + MARGIN)
+                    # Set that location to one
+                    grid[row][column] = 1
+                    print("Click ", pos, "Grid coordinates: ", row, column)                
+ 
+            # Redraw the background
+            self.screen.fill(self.bg_color)
+ 
+            for name, label, (width, height), (posx, posy) in self.items:
+                self.screen.blit(label, (posx, posy))
+
+                              
+ 
+            pygame.display.flip()
+ 
+ 
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+ 
+    menu_items = ('Start', 'Quit')
+ 
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen, menu_items)
+    gm.run()
+               
+                    
